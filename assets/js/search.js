ace271453f7f2b81ad247c2e8cb19cc5f61dc1e7
Alexei Danchenkov <adanchenkov@emailline.net>
2024-07-10 00:07:49 +0300

    potentially breaking change

--- assets/js/search.js
+++ assets/js/search.js
@@ -9,89 +9,109 @@ const fuseOptions = {
     maxPatternLength: 32,
     minMatchCharLength: 1,
     keys: [
-        { name: 'title', weight: 0.8 }, { name: 'contents', weight: 0.5 }, { name: 'tags', weight: 0.3 }, { name: 'categories', weight: 0.3 }
+        { name: 'title', weight: 0.4 }, { name: 'contents', weight: 0.3 }, { name: 'tags', weight: 0.25 }, { name: 'categories', weight: 0.15 }
     ]
 }
 
-const searchQuery = param('s')
-if (searchQuery) {
-    $('#search-query').val(searchQuery)
-    executeSearch(searchQuery)
-} else {
-    $('#search-results').append('<p>Введите слово или фразу</p>')
+let inputBox = document.getElementById('search-query')
+if (inputBox !== null) {
+    let searchQuery = param("q")
+    let searchResults = document.getElementById('search-results')
+    if (searchQuery) {
+        inputBox.value = searchQuery || ""
+        executeSearch(searchQuery, false)
+    } else if (searchResults) {
+        searchResults.innerHTML = '<p class="search-results-empty">Please enter a word or phrase above, or see <a href="/tags/">all tags</a>.</p>'
+    }
 }
 
 function executeSearch(searchQuery) {
-    $.getJSON('/index.json', function (data) {
-        const pages = data
-        const fuse = new Fuse(pages, fuseOptions)
-        const result = fuse.search(searchQuery)
-        console.log({ matches: result })
-        if (result.length > 0) {
-            populateResults(result)
-        } else {
-            $('#search-results').append('<p>Поиск результатов не дал…</p>')
+
+    show(document.querySelector('.search-loading'))
+
+    fetch('/index.json').then(function (response) {
+        if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' + response.status)
+            return
         }
+        // Examine the text in the response
+        response.json().then(function (pages) {
+            let fuse = new Fuse(pages, fuseOptions)
+            let result = fuse.search(searchQuery)
+            if (result.length > 0) {
+                populateResults(result)
+            } else {
+                document.getElementById('search-results').innerHTML = '<p class="search-results-empty">No matches found</p>'
+            }
+            hide(document.querySelector('.search-loading'))
+        })
+            .catch(function (err) {
+                console.log('Fetch Error :-S', err)
+            })
     })
 }
 
-function populateResults(result) {
-    $.each(result, function (key, value) {
-        const contents = value.item.contents
-        let snippet = ''
-        const snippetHighlights = []
-        // const tags = []
-        let start, end
-        if (fuseOptions.tokenize) {
-            snippetHighlights.push(searchQuery)
-        } else {
-            $.each(value.matches, function (matchKey, mvalue) {
-                if (mvalue.key === 'tags' || mvalue.key === 'categories') {
-                    snippetHighlights.push(mvalue.value)
-                } else if (mvalue.key === 'contents') {
-                    start = mvalue.indices[0][0] - summaryInclude > 0 ? mvalue.indices[0][0] - summaryInclude : 0
-                    end = mvalue.indices[0][1] + summaryInclude < contents.length ? mvalue.indices[0][1] + summaryInclude : contents.length
-                    snippet += contents.substring(start, end)
-                    snippetHighlights.push(mvalue.value.substring(mvalue.indices[0][0], mvalue.indices[0][1] - mvalue.indices[0][0] + 1))
-                }
+function populateResults(results) {
+
+    let searchQuery = document.getElementById("search-query").value
+    let searchResults = document.getElementById("search-results")
+
+    // pull template from hugo template definition
+    let templateDefinition = document.getElementById("search-result-template").innerHTML
+
+    results.forEach(function (value, key) {
+
+        let contents = value.item.contents
+        let snippet = ""
+        let snippetHighlights = []
+
+        snippetHighlights.push(searchQuery)
+        snippet = contents.substring(0, summaryInclude * 2) + '&hellip;'
+
+        //replace values
+        let tags = ""
+        if (value.item.tags) {
+            value.item.tags.forEach(function (element) {
+                tags = tags + "<a href='/tags/" + element + "'>" + "#" + element + "</a> "
             })
         }
 
-        if (snippet.length < 1) {
-            snippet += contents.substring(0, summaryInclude * 2)
-        }
-        // pull template from hugo templarte definition
-        const templateDefinition = $('#search-result-template').html()
-        // replace values
-        const output = render(templateDefinition, { key: key, title: value.item.title, link: value.item.permalink, tags: value.item.tags, categories: value.item.categories, snippet: snippet })
-        $('#search-results').append(output)
-
-        $.each(snippetHighlights, function (snipkey, snipvalue) {
-            $('#summary-' + key).mark(snipvalue)
+        let output = render(templateDefinition, {
+            key: key,
+            title: value.item.title,
+            link: value.item.permalink,
+            tags: tags,
+            categories: value.item.categories,
+            snippet: snippet
         })
+        searchResults.innerHTML += output
+
+        snippetHighlights.forEach(function (snipvalue, snipkey) {
+            var instance = new Mark(document.getElementById('summary-' + key))
+            instance.mark(snipvalue)
+            let s = snipkey
+            snipkey = s
+        })
+
     })
 }
 
-function param(name) {
-    return decodeURIComponent((location.search.split(name + '=')[1] || '').split('&')[0]).replace(/\+/g, ' ')
-}
-
 function render(templateString, data) {
-    let conditionalMatches, copy
-    const conditionalPattern = /\$\{\s*isset ([a-zA-Z]*) \s*\}(.*)\$\{\s*end\s*}/g
-    // since loop below depends on re.lastInxdex, we use a copy to capture any manipulations whilst inside the loop
+    let conditionalMatches, conditionalPattern, copy
+    conditionalPattern = /\$\{\s*isset ([a-zA-Z]*) \s*\}(.*)\$\{\s*end\s*}/g
+    //since loop below depends on re.lastInxdex, we use a copy to capture any manipulations whilst inside the loop
     copy = templateString
     while ((conditionalMatches = conditionalPattern.exec(templateString)) !== null) {
         if (data[conditionalMatches[1]]) {
-            // valid key, remove conditionals, leave contents.
+            //valid key, remove conditionals, leave contents.
             copy = copy.replace(conditionalMatches[0], conditionalMatches[2])
         } else {
-            // not valid, remove entire section
+            //not valid, remove entire section
             copy = copy.replace(conditionalMatches[0], '')
         }
     }
     templateString = copy
-    // now any conditionals removed we can do simple substitution
+    //now any conditionals removed we can do simple substitution
     let key, find, re
     for (key in data) {
         find = '\\$\\{\\s*' + key + '\\s*\\}'
@@ -99,4 +119,15 @@ function render(templateString, data) {
         templateString = templateString.replace(re, data[key])
     }
     return templateString
+}
+
+// Helper Functions
+function show(elem) {
+    if (elem) { elem.style.display = 'block' }
+}
+function hide(elem) {
+    if (elem) { elem.style.display = 'none' }
+}
+function param(name) {
+    return decodeURIComponent((location.search.split(name + '=')[1] || '').split('&')[0]).replace(/\+/g, ' ')
 }
